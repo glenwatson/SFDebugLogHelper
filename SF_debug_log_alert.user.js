83e45eb5452da0daf917f705c46e24e806751ecd
Glen <glen.watson@alumni.neumont.edu>
2013-02-11 00:21:27 -0500

    added filtering

--- SF_debug_log_alert.user.js
+++ SF_debug_log_alert.user.js
@@ -7,8 +7,22 @@
 // @grant       none
 // ==/UserScript==
 
-var btn = document.createElement('button');
-btn.setAttribute('onClick', "function htmlDecode(e){var t=document.createElement('div');t.innerHTML=e;return t.childNodes.length===0?'':t.childNodes[0].nodeValue}var lines=document.getElementsByTagName('pre')[0].innerHTML.split('\\n');for(var i=0;i<lines.length;i++){var lineParts=lines[i].split('|');if(lineParts[1]=='USER_DEBUG'){alert(htmlDecode(lineParts[4]))}}");
-btn.innerHTML = 'alert DEBUGs';
-var inserted = document.getElementsByTagName('body')[0].insertBefore(btn, document.getElementsByTagName('pre')[0]);
+var script = document.createElement('script');
+script.setAttribute('type', 'text/javascript');
+script.innerHTML = 'function searchLog(){function e(e){var t=document.createElement("div");t.innerHTML=e;return t.childNodes.length===0?"":t.childNodes[0].nodeValue}var t=document.getElementsByTagName("pre")[0].innerHTML.split("\\n");var n=document.getElementById("filter");var r=n!=null?n.value:"";for(var i=0;i<t.length;i++){var s=t[i].split("|");if(s[1]=="USER_DEBUG"&&s[4].indexOf(r)!=-1){alert(e(s[4]))}}}';
+document.getElementsByTagName('head')[0].appendChild(script);
 
+
+var btn = document.createElement('button');
+btn.setAttribute('onClick', 'searchLog();');
+btn.innerHTML = 'alert DEBUGs';
+document.getElementsByTagName('body')[0].insertBefore(btn, document.getElementsByTagName('pre')[0]);
+
+if(true)
+{
+	var filter = document.createElement('input');
+	filter.setAttribute('type', 'text');
+	filter.setAttribute('id', 'filter');
+	filter.setAttribute('onkeydown', 'if(event.keyCode == 13) {searchLog();}');
+	document.getElementsByTagName('body')[0].insertBefore(filter, document.getElementsByTagName('pre')[0]);
+}
--- logParser.js
+++ logParser.js
@@ -1,15 +1,19 @@
-// http://stackoverflow.com/questions/1912501/unescape-html-entities-in-javascript
 // http://jscompress.com/
-function htmlDecode(input) {
-	var e = document.createElement('div');
-	e.innerHTML = input;
-	return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
-}
+function searchLog() {
+	// http://stackoverflow.com/questions/1912501/unescape-html-entities-in-javascript
+	function htmlDecode(input) {
+		var e = document.createElement('div');
+		e.innerHTML = input;
+		return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+	}
 
-var lines = document.getElementsByTagName('pre')[0].innerHTML.split('\n')
-for (var i = 0; i < lines.length; i++) {
-	var lineParts = lines[i].split('|');
-	if(lineParts[1] == 'USER_DEBUG') {
-		alert(htmlDecode(lineParts[4]));
+	var lines = document.getElementsByTagName('pre')[0].innerHTML.split('\\n');
+	var filterEle = document.getElementById('filter');
+	var filterText = (filterEle != null) ? filterEle.value : '';
+	for (var i = 0; i < lines.length; i++) {
+		var lineParts = lines[i].split('|');
+		if(lineParts[1] == 'USER_DEBUG' && lineParts[4].indexOf(filterText) != -1) {
+			alert(htmlDecode(lineParts[4]));
+		}
 	}
 }
